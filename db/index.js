47afda031c0d89d3151fa675c6c89607b811a0ff
John <jvt703@gmail.com>
2021-09-07 16:59:03 -0400

    seed/index updates

--- db/index.js
+++ db/index.js
@@ -15,10 +15,24 @@ async function getAllUsers() {
 }
 
 
-
-
+async function createUser({ username, password }) {
+  try {
+    const {rows} = await client.query(`
+      INSERT INTO users(username, password) 
+      VALUES($1, $2) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password]);
+    
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = {
-  client, getAllUsers,
+  client,
+  getAllUsers,
+  createUser,
 }
 
