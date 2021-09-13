95f15c37526ab58a979cf7443e955c39902f5727
John <jvt703@gmail.com>
2021-09-12 21:46:45 -0400

    db connection change

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const { Client } = require('pg'); // imports the pg module
 require('dotenv').config();
 // supply the db name and location of the database
 
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL || process.env.Connection);
 
 async function getAllUsers() {
   const { rows } = await client.query(
