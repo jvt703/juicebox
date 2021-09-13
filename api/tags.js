e1750f7e19b85acd86e0ebd79e37e9f3a78d9a41
John <jvt703@gmail.com>
2021-09-12 22:29:31 -0400

    updated filter in tags

--- api/tags.js
+++ api/tags.js
@@ -26,12 +26,14 @@ tagsRouter.get('/:tagName/posts', async (req, res, next) => {
     const allposts = await getPostsByTagName( tagName)
     // use our method to get posts by tag name from the db
     // send out an object to the client { posts: // the posts }
+    console.log(allposts,"ALLL")
+    console.log(req.user[0].id, 'herrrrr')
     const posts =  allposts.filter(
       (post) => {
-        return post.active && (req.user && post.author.id === req.user.id); 
+        return post.active && (req.user && post.author.id === req.user[0].id); 
       }
     )
-
+    console.log(posts,"filtered")
     res.send({posts: posts})
   } catch ({ name, message }) {
     // forward the name and message to the error handler
